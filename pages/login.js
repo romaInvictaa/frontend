2347942a0d3fc1dc89d09df53254dd4bb45ce184
Juan Esteban <juan.betancourt.narvaez@correounivalle.edu.co>
2023-05-22 18:00:00 -0500

    Login page
    Página de login diseñada acorde a el modelo original
    
    - Incluida la imagen

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext.jsx";
+import Link from "next/link";
 import Image from "next/image";
 
 const LoginForm = () => {
   const auth = useAuth();
-  console.log(auth);
+  // console.log(auth);
   const { displayName } = auth.user;
-  console.log(displayName);
+  // console.log(displayName);
   const usuariomail = auth.user.email;
-  console.log(usuariomail);
+  // console.log(usuariomail);
   const { login, loginWithGoogle } = useAuth();
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
@@ -39,17 +40,23 @@ const LoginForm = () => {
     auth.logout();
   };
 
+
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  function togglePasswordVisibility() {
+    setIsPasswordVisible((prevState) => !prevState);
+  }
+
   return (
-    <div className="container p-8 rounded-3xl bg-gray-500 mt-10 ml-10 mr-10">
-      <div className="grid grid-cols-12 gap-12 ml-10">
-        <div className="lg:col-span-6 sm:col-span-12">
-          {displayName && <h1>Bienvenido {displayName}</h1>}
-          <h2>Iniciar sesión</h2>
-          <form onSubmit={handleLogin}>
+    <div className="container">
+      <div className="grid grid-cols-12 gap-12 bg-white w-fit min-[530px]:w-full sm:w-screen">
+        <div className="col-span-12 lg:col-span-6 py-24 px-10 lg:px-20 lg:py-10 xl:px-28">
+          <h1 className="text-4xl font-semibold mb-8">Iniciar sesión</h1>
+          <form onSubmit={handleLogin} className="align-middle align-center">
             <div>
-              <label htmlFor="login-email">Correo electrónico:</label>
+              <label className='text-lg' htmlFor="login-email">Correo electrónico</label>
               <input
-                className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+                className="mt-2 p-4 outline outline-gray-400 outline-1 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 focus:bg-gray-100 bg-white text-gray-700 mb-8"
                 type="email"
                 id="login-email"
                 required
@@ -57,41 +64,101 @@ const LoginForm = () => {
                 onChange={handleLoginEmailChange}
               />
             </div>
-            <div>
-              <label htmlFor="login-password">Contraseña:</label>
+            <div className="relative">
+              <label className='text-lg' htmlFor="login-password">Contraseña</label>
               <input
-                className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+                className="mt-2 p-4 outline outline-gray-400 outline-1 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 focus:bg-gray-100 bg-white text-gray-700 mb-8"
                 required
-                type="password"
+                type={isPasswordVisible ? "text" : "password"}
                 id="login-password"
                 value={loginPassword}
                 onChange={handleLoginPasswordChange}
               />
+
+              <button
+                className="absolute inset-y-0 right-0 flex items-center px-4 text-gray-600"
+                onClick={togglePasswordVisibility}
+              >
+                {isPasswordVisible ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                )}
+              </button>
             </div>
             <button
-              className="mt-2 ml-5 mb-5 align-middle align-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="w-full mb-4 bg-orange-primary hover:bg-orange-secondary transition duration-500 text-white px-4 py-3 rounded-lg text-lg"
               type="submit"
             >
               Iniciar sesión
             </button>
-            <button
-              onClick={handleLogout}
-              className="mt-2 ml-5 mb-5 align-middle align-center bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Logout
-            </button>
+
           </form>
-          <div className="w-full flex justify-center items-center">
-            <button
-              className="mb-5 align-middle align-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={handleGoogleLogin}
-            >
-              Iniciar sesión con Google
-            </button>
+
+
+
+          <button
+            className="w-full bg-white hover:bg-gray-100 transition duration-500 text-black outline outline-gray-400 outline-1 px-4 py-3 rounded-lg text-lg mb-4"
+            onClick={handleGoogleLogin}
+          >
+            <div className="grid grid-cols-12">
+              <div className="col-span-2 flex justify-end">
+                <img src="/googleLogo.png" className="h-7 w-7 inline text-pink-500" width={20} alt="google" />
+              </div>
+              <div className="col-span-8">
+                <span>Iniciar sesión con Google</span>
+              </div>
+            </div>
+          </button>
+
+
+          <div className="mt-2 mb-2 flex justify-center">
+            <Link href="/recover">
+              <span className="text-orange-primary hover:underline">¿Olvidaste tu contraseña?</span>
+            </Link>
           </div>
+
+          <div className="mt-2 mb-2 flex justify-center">
+            <span href='/recover'>¿No tienes cuenta?&nbsp;</span>
+            <Link href="/register">
+              <span className="text-orange-primary hover:underline">Registrate</span>
+            </Link>
+          </div>
+
         </div>
-        <div className="lg:col-span-6 sm:col-span-12 h-full">
-          <img src="/bg2.png" width={350}  alt="login" />
+        <div className="invisible lg:visible lg:col-span-6 flex justify-end">
+          <img className="h-screen" src="/loginImg.png" alt="login" />
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'orange-primary': '#FE8660',
+        'orange-secondary': '#FA6B43',
+      }
     },
   },
   plugins: [],
