439a280bd99f3f289768f9228d6197d62ba80f5b
rwxp <sdrivert@hotmail.com>
2023-05-24 23:31:16 -0500

    Attempted consolidation

---
+++ three/src/Video.jsx
@@ -0,0 +1,27 @@
+import { useVideoTexture } from '@react-three/drei';
+import { DoubleSide } from 'three';
+import { useState } from 'react';
+
+export default function Video() {
+  const [pause, setPause] = useState(false);
+
+  const props = {
+    unsuspend: 'canplay',
+    muted: true,
+    loop: true,
+    start: pause,
+  };
+
+  const videoTexture = useVideoTexture('.//video/cielo.mp4', props);
+
+  const playVideo = () => {
+    setPause(true);
+  };
+
+  return (
+    <mesh receiveShadow onClick={playVideo} position={[0, 15, 0]}>
+      <sphereGeometry args={[50, 16, 20, 0, 6.4, 1, 1.2]} />
+      <meshStandardMaterial map={videoTexture} side={DoubleSide} />
+    </mesh>
+  );
+}
