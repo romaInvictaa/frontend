8709d12a90ff9186a8a8a920c4f44682562616da
Juan Esteban <juan.betancourt.narvaez@correounivalle.edu.co>
2023-05-25 00:07:17 -0500

    Responsive design
    La página fue probada en varios dispositivos, es completamente responsive

--- components/MobileHeader.jsx
+++ components/MobileHeader.jsx
@@ -1,34 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 const Header = ({ user }) => {
 
-    const [showMenu, setShowMenu] = useState(false);
-
     return (
-        <div className={showMenu ? "container mx-auto bg-dark-slate-blue" : "container mx-auto"}>
-            <div className="md:border-b w-full inline-block border-dark-slate-blue py-2 px-12 xl:px-16">
-                <div className="float-right align-middle translate-y-3 md:hidden" onClick={() => setShowMenu(!showMenu)}>
+        <div className="container mx-auto px-12 xl:px-16">
+            <div className="md:border-b w-full inline-block border-dark-slate-blue py-2">
 
-                    {showMenu ? (
-                        <div className='translate-x-2 -translate-y-1'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="#E5E5E5" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                            </svg>
-                        </div>
-                    ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
-                        </svg>
-                    )
-                    }
-
-
-
-
+                <div className="float-right align-middle translate-y-3 md:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <line x1="3" y1="12" x2="21" y2="12"></line>
+                        <line x1="3" y1="6" x2="21" y2="6"></line>
+                        <line x1="3" y1="18" x2="21" y2="18"></line>
+                    </svg>
                 </div>
+
                 {user ? (
                     <div className="hidden md:float-left md:contents">
                         <img
@@ -60,19 +46,16 @@ const Header = ({ user }) => {
                     </div>
                 )}
             </div>
-
             {showMenu ? (
-                <div className="md:hidden bg-dark-slate-blue pb-4 mb-4">
-                    <div className="flex justify-center">
+                <div className="md:hidden bg-dark-slate-blue w-screen -translate-x-12">
+                    <div className="flex inline-block">
                         <Link href="/login">
-                            <span className="text-light-gray font-semibold text-2xl cursor-pointer hover:text-cream-primary transition duration-300">
+                            <span className="text-light-gray font-semibold text-lg cursor-pointer hover:text-cream-primary transition duration-300">
                                 Ingresar
                             </span>
                         </Link>
-                    </div>
-                    <div className="flex justify-center mt-4">
                         <Link href="/register">
-                            <span className="text-light-gray font-semibold text-2xl cursor-pointer hover:text-cream-primary transition duration-300">
+                            <span className="text-light-gray font-semibold text-lg cursor-pointer hover:text-cream-primary transition duration-300">
                                 Regístrate
                             </span>
                         </Link>
@@ -81,7 +64,6 @@ const Header = ({ user }) => {
             ) : (
                 <div></div>
             )}
-
         </div>
     )
 }
