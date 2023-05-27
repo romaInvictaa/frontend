3f2f44802e20b7a96ab73b76ee27732efeb663b5
Juan Esteban <juan.betancourt.narvaez@correounivalle.edu.co>
2023-05-27 01:56:36 -0500

    Final updates

--- pages/architecture/pantheon.js
+++ pages/architecture/pantheon.js
@@ -22,14 +22,14 @@ const Pantheon = () => {
         </div>
 
         <div className="grid grid-cols-6">
-          <div className="col-span-6  md:col-span-4 border-8 border-dark-slate-blue rounded-3xl bg-blue-200 mb-8">
+          <div className="col-span-6 md:col-span-4 border-8 border-dark-slate-blue rounded-3xl bg-blue-200 mb-8">
             <Canvas camera={cameraSettings} shadows={true} className='rounded-2xl'>
               <PanteonExperience />
             </Canvas>
           </div>   
           <div className="col-span-6 md:col-span-2 sm:px-6">
             <InfoCard text={text}/>
-            <ImageCarousel />
+            <ImageCarousel images={images}/>
           </div>
       </div>
       </div>
@@ -39,4 +39,5 @@ const Pantheon = () => {
 
 export default Pantheon;
 
-const text = "Un antiguo templo romano, situado en la ciudad de Roma, Italia. Fue terminado por orden del emperador Adriano y dedicado alrededor del año 126. Su fecha de construcción es incierta porque Adriano optó por no inscribir el nuevo templo, sino que conservó la inscripción del templo más antiguo que se había quemado."
+const text = "Un antiguo templo romano. Fue terminado por orden del emperador Adriano y dedicado alrededor del año 126. Su fecha de construcción es incierta porque Adriano optó por no inscribir el nuevo templo, sino que conservó la inscripción del templo más antiguo que se había quemado."
+const images = ["/pantheon/pantheon1.png", "/pantheon/pantheon2.png", "/pantheon/pantheon3.png", "/pantheon/pantheon4.png", "/pantheon/pantheon5.png"];
