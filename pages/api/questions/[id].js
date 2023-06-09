d0539362b0021bb8b7703b8315352a65aefadd75
rwxp <sdrivert@hotmail.com>
2023-06-08 21:15:16 -0500

    Questions retrieving

--- pages/api/questions/[id].js
+++ pages/api/questions/[id].js
@@ -12,12 +12,10 @@ const pool = new Pool({
 
 export default async function getQuestionById(req, res) {
   if (req.method === 'GET') {
-    //console.log("req.body", req.body);
-    const id = req.params.id;
+    const { id } = req.query;
     const response = await pool.query('SELECT * FROM questions WHERE id = $1', [
       id,
     ]);
-    //console.log("response", response);
     res.status(200).json(response.rows);
   } else {
     res.status(404).json({ name: 'Method not defined' });
--- pages/architecture/testarchitecture.js
+++ pages/architecture/testarchitecture.js
@@ -1,23 +1,56 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Header, PageCard } from '@/components';
 import Link from 'next/link';
 
-const QUESTIONS_URL = '/api/questions/';
+const QUESTIONS_URL = '/api/questions';
 
 const testarchitecture = () => {
-  const [respuestas, setRespuestas] = useState([]);
-  // const [numeroPregunta, setNumeroPregunta] = useState([]);
+  const [numeroPregunta, setNumeroPregunta] = useState(1);
+  const [seleccionada, setSeleccionada] = useState(null);
+  const [question, setQuestion] = useState('');
+  const [option1, setOption1] = useState('');
+  const [option2, setOption2] = useState('');
+  const [option3, setOption3] = useState('');
+  const [option4, setOption4] = useState('');
+  const [actualizarInfo, setActualizarInfo] = useState(true);
+  const [answer, setAnswer] = useState('');
   const opciones = [
-    { id: 1, texto: 'a) Entrenamiento militar' },
-    { id: 2, texto: 'b) Natación' },
-    { id: 3, texto: 'c) Debates políticos' },
-    { id: 4, texto: 'd) Entretenimiento ' },
+    { id: 1, texto: option1 },
+    { id: 2, texto: option2 },
+    { id: 3, texto: option3 },
+    { id: 4, texto: option4 },
   ];
 
   const handleRespuesta = (opcionId) => {
-    setRespuestas([opcionId]);
+    setSeleccionada(opcionId);
   };
+  const handleEnvio = () => {
+    setNumeroPregunta(numeroPregunta + 1);
+    console.log(answer);
+    if (opciones[seleccionada - 1].texto == answer) {
+      alert('Todo bien manito');
+    } else {
+      alert('Se equivocó perro hpta');
+    }
+    setActualizarInfo(true);
+    setSeleccionada(null);
+  };
+  const getQuestion = async (id) => {
+    const response = await fetch(`${QUESTIONS_URL}/${id}`);
+    const ans = await response.json();
+    setQuestion(ans[0].question);
+    setOption1(ans[0].option1);
+    setOption2(ans[0].option2);
+    setOption3(ans[0].option3);
+    setOption4(ans[0].option4);
+    setAnswer(ans[0].answer);
+  };
+  useEffect(() => {
+    getQuestion(numeroPregunta);
+    setActualizarInfo(false);
+  }, [actualizarInfo]);
+
   return (
     <>
       <Header slug={'/architecture'} />
@@ -30,7 +63,7 @@ const testarchitecture = () => {
         >
           <div className="container mx-auto">
             <h1 className="text-3xl mx-8 mt-8 font-semibold text-dark-slate-blue flex justify-center xl:text-xl xl:mb-2">
-              Cual era el propósito principal del Coliseo Romano?
+              {question}
             </h1>
             <div className="col-span-2 ml-12">
               <img
@@ -51,7 +84,7 @@ const testarchitecture = () => {
                   <input
                     type="radio" // Cambiar el tipo de checkbox a radio
                     className="form-radio h-3 w-5 text-blue-500"
-                    checked={opcion.id === respuestas[0]} // Comprobar si la opción es igual a la opción seleccionada
+                    checked={opcion.id === seleccionada} // Comprobar si la opción es igual a la opción seleccionada
                     onChange={() => handleRespuesta(opcion.id)}
                   />
                   <span className="ml-2 text-lg">{opcion.texto}</span>
@@ -60,16 +93,15 @@ const testarchitecture = () => {
             </div>
 
             <div className="flex justify-between items-center mb-8 ml-3 mr-8 ">
-              <Link href="/">
-                <span className="md:float-left  mt-2 align-middle text-cream-primary ml-4 font-semibold cursor-pointer bg-dark-slate-blue border border-dark-slate-blue rounded-full px-8 py-2 transition duration-300 hover:bg-cream-primary hover:text-dark-slate-blue hover:border-cream-primary">
-                  Siguiente Pregunta
-                </span>
-              </Link>
-              <Link href="/">
-                <span className="md:float-left  mt-2 align-middle text-cream-primary ml-4 font-semibold cursor-pointer bg-dark-slate-blue border border-dark-slate-blue rounded-full px-8 py-2 transition duration-300 hover:bg-cream-primary hover:text-dark-slate-blue hover:border-cream-primary">
-                  Enviar
-                </span>
-              </Link>
+              <span
+                onClick={() => handleEnvio()}
+                className="md:float-left  mt-2 align-middle text-cream-primary ml-4 font-semibold cursor-pointer bg-dark-slate-blue border border-dark-slate-blue rounded-full px-8 py-2 transition duration-300 hover:bg-cream-primary hover:text-dark-slate-blue hover:border-cream-primary"
+              >
+                Siguiente Pregunta
+              </span>
+              <span className="md:float-left  mt-2 align-middle text-cream-primary ml-4 font-semibold cursor-pointer bg-dark-slate-blue border border-dark-slate-blue rounded-full px-8 py-2 transition duration-300 hover:bg-cream-primary hover:text-dark-slate-blue hover:border-cream-primary">
+                Enviar
+              </span>
             </div>
           </div>
         </div>
