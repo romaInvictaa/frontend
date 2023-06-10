f02ff3c844a7ec43113df5b883ec2c8c0777c244
Juan Esteban <juan.betancourt.narvaez@correounivalle.edu.co>
2023-06-10 00:32:14 -0500

    Despliegue Produccion, Sprint 2

--- pages/architecture/index.js
+++ pages/architecture/index.js
@@ -14,13 +14,13 @@ const History = () => {
           {/* <div className="ml-64">
                         <span className='text-md lg:text-xl text-orange-primary font-semibold'>TE PRESENTAMOS</span>
                     </div> */}
-          <div className="flex justify-center py-2">
+          <div className="flex justify-center py-2" data-testid="title">
             <span className="text-5xl md:text-4xl lg:text-5xl font-semibold mb-4 text-dark-slate-blue">
               Tesoros de la antigua Roma
             </span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-full">
+        <div className="grid grid-cols-2 w-full" data-testid="cards">
           {pages.map((page, index) => (
             <div className="col-span-2 md:col-span-1 sm:px-6" key={index}>
               <PageCard page={page} />
