465ba60bf157d344b395e8d37867a1bed27850a9
KPOETA1 <geider.montano@correounivalle.edu.co>
2023-07-07 20:17:47 -0500

    Boton mas informacion
    v1

--- pages/art/primaporta.js
+++ pages/art/primaporta.js
@@ -1,6 +1,7 @@
-import { Canvas } from '@react-three/fiber';
-import Experience from '../../three/src/PrimaPortaExperience';
-import { Header, InfoCard, ImageCarousel } from '@/components';
+import { Canvas } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import Experience from "../../three/src/PrimaPortaExperience";
+import { Header, InfoCard, ImageCarousel } from "@/components";
 
 const Primaporta = () => {
   const cameraSettings = {
@@ -10,28 +11,62 @@ const Primaporta = () => {
     far: 400,
     position: [-2, 21, 20],
   };
+  const [showInfo, setShowInfo] = useState(false);
+
+  const toggleInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
+  const infoUrl = "https://es.wikipedia.org/wiki/Augusto";
 
   return (
     <>
-      <Header slug={"/art"}/>
+      <Header slug={"/art"} />
       <div className="container mx-auto px-6 md:px-0">
         <div className="grid grid-cols-1">
           <div className="flex justify-center py-2">
-            <span className='text-5xl md:text-5xl lg:text-5xl font-semibold mb-4 text-dark-slate-blue py-2'>Augusto Prima Porta</span>
+            <span className="text-5xl md:text-5xl lg:text-5xl font-semibold mb-4 text-dark-slate-blue py-2">
+              Augusto Prima Porta
+            </span>
           </div>
         </div>
 
         <div className="grid grid-cols-6">
           <div className="col-span-6 md:col-span-4 border-8 border-dark-slate-blue rounded-3xl bg-blue-200 mb-8">
-            <Canvas camera={cameraSettings} shadows={true} className='rounded-2xl'>
+            <Canvas
+              camera={cameraSettings}
+              shadows={true}
+              className="rounded-2xl"
+            >
               <Experience />
             </Canvas>
-          </div>   
-          <div className="col-span-6 md:col-span-2 sm:px-6">
-            <InfoCard text={text}/>
-            <ImageCarousel images={images} />
           </div>
-      </div>
+          <div className="col-span-6 md:col-span-2 sm:px-6">
+            <InfoCard text={text} />
+            <ImageCarousel images={images} />
+            <div className="flex justify-center mb-8">
+              {showInfo && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+                  <div className="bg-white rounded-lg w-full h-full my-36 mb-12 p-2 mx-2 md:p-4  md:w-2/3 md:h-5/6 md:my-24">
+                    <button
+                      className="absolute top-2 right-2 text-3xl text-gray-500 hover:text-gray-700"
+                      onClick={toggleInfo}
+                    >
+                      X
+                    </button>
+                    <iframe className="w-full h-full" src={infoUrl}></iframe>
+                  </div>
+                </div>
+              )}
+              <button
+                className="bg-dark-slate-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                onClick={toggleInfo}
+              >
+                Más información
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
@@ -39,5 +74,11 @@ const Primaporta = () => {
 
 export default Primaporta;
 
-const text = "La estatua es una imagen idealizada de Augusto que se basa en el Doríforo de Policleto del siglo V a. C. Acoge la forma de contrapposto de esa escultura, creando diagonales entre los miembros tensos y los relajados, es un rasgo típico de la escultura clásica."
-const images = ["/primaporta/primaporta1.jpg", "/primaporta/primaporta2.jpg", "/primaporta/primaporta3.jpeg", "/primaporta/primaporta4.png"];
+const text =
+  "La estatua es una imagen idealizada de Augusto que se basa en el Doríforo de Policleto del siglo V a. C. Acoge la forma de contrapposto de esa escultura, creando diagonales entre los miembros tensos y los relajados, es un rasgo típico de la escultura clásica.";
+const images = [
+  "/primaporta/primaporta1.jpg",
+  "/primaporta/primaporta2.jpg",
+  "/primaporta/primaporta3.jpeg",
+  "/primaporta/primaporta4.png",
+];
